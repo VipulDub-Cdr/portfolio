6373d86c4f9ca101c0b5b3dbf07ed10611a0488a
VipulDub-Cdr <vipuldubey041@gmail.com>
2025-07-18 12:26:50 +0530

    educ

--- src/constants/education.ts
+++ src/constants/education.ts
@@ -1,16 +1,6 @@
 import { ExperienceType } from "../types";
 
 export const experiences: ExperienceType[] = [
-  // {
-  //   id: 1,
-  //   company: "LinuxWorld Informatics Pvt Ltd",
-  //   companyUrl: "https://www.linkedin.com/company/linuxworld-informatics-pvt-ltd/posts/?feedView=all",
-  //   designation: "DevOps Intern",
-  //   startDate: "June, 2025",
-  //   endDate: "Present",
-  //   description:
-  //     "Configured and managed Apache HTTP Server on RHEL with secure SSH and systemd services. Handled Linux user access with proper sudo policies. Implemented Docker-in-Docker for CI/CD testing. Collaborated on infrastructure automation in a 4-member team and optimized deployment workflows with strong attention to detail.",
-  // },
   {
     id: 1,
     company: "Oriental Institute of Science & Technology, Bhopal ",
@@ -19,7 +9,7 @@ export const experiences: ExperienceType[] = [
     startDate: "Sep, 2022",
     endDate: "Present",
     description:
-      "<CGPA: 8.11/10 (till 5th semester)/>",
+      "<CGPA: 8.11/10 />",
   },
   {
     id: 2,
@@ -29,16 +19,16 @@ export const experiences: ExperienceType[] = [
     startDate: "2020",
     endDate: "2021",
     description:
-      "<Percentage: 81% />",
+      "<12th Grade: 81% />",
   },
-  // {
-  //   id: 3,
-  //   company: "Marktine Technology Solutions Pvt Ltd",
-  //   companyUrl: "https://www.marktine.com/",
-  //   designation: "Full Stack Intern",
-  //   startDate: "Mar, 2023",
-  //   endDate: "Aug, 2023",
-  //   description:
-  //     "Developed RESTful APIs using Node.js and Express.js, built responsive web applications with Next.js, implemented test cases and logging systems. Managed MySQL database operations and worked in Linux environments.",
-  // },
+  {
+    id: 3,
+    company: "Samaritans English Medium Higher Secondary School",
+    companyUrl: "",
+    designation: "",
+    startDate: "2018",
+    endDate: "2019",
+    description:
+      "<10th Grade: 91.4% />",
+  },
 ];
